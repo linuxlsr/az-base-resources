3c7515e7d38414e19ed7579592a7a9e8ea8a9cde
Charles Harvey <soops@ucla.edu>
2020-11-04 14:54:27 -0800

    removing mysql to stand up other resources

--- mysql.tf
+++ mysql.tf
@@ -1,9 +1,9 @@
-resource "azurerm_mysql_server" "mysql" {
-  location = var.location
-  name = var.db_server_name
-  resource_group_name = var.resource_group_name
-  administrator_login = var.admin_username
-  administrator_login_password = var.db_password
+//resource "azurerm_mysql_server" "mysql" {
+//  location = var.location
+//  name = var.db_server_name
+//  resource_group_name = var.resource_group_name
+//  administrator_login = var.admin_username
+//  administrator_login_password = var.db_password
 //  sku_name = var.sku_name
 //  version = var.db_version
 //  sku {
@@ -12,12 +12,12 @@ resource "azurerm_mysql_server" "mysql" {
 //    tier     = "Basic"
 //    family = "Gen5"
 //  }
-
-  storage_profile {
-    storage_mb            = 5120
-    backup_retention_days = 7
-    geo_redundant_backup  = "Disabled"
-  }
+//
+//  storage_profile {
+//    storage_mb            = 5120
+//    backup_retention_days = 7
+//    geo_redundant_backup  = "Disabled"
+//  }
 //  ssl_enforcement           = var.ssl_enforcement_enabled
-//  ssl_minimal_tls_version_enforced  = var.ssl_minimal_tls_version_enforced
-}
+////  ssl_minimal_tls_version_enforced  = var.ssl_minimal_tls_version_enforced
+//}
