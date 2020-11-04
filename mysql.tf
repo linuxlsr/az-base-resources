83c9c844b946b064ea626392737a94d2e051d21e
Charles Harvey <soops@ucla.edu>
2020-11-04 14:42:20 -0800

    testing omission of vars per tg validate take 6

--- mysql.tf
+++ mysql.tf
@@ -4,8 +4,8 @@ resource "azurerm_mysql_server" "mysql" {
   resource_group_name = var.resource_group_name
   administrator_login = var.admin_username
   administrator_login_password = var.db_password
-  sku_name = var.sku_name
-  version = var.db_version
+//  sku_name = var.sku_name
+//  version = var.db_version
 //  sku {
 //    name     = "B_Gen5_2"
 //    capacity = 2
@@ -18,6 +18,6 @@ resource "azurerm_mysql_server" "mysql" {
     backup_retention_days = 7
     geo_redundant_backup  = "Disabled"
   }
-  ssl_enforcement           = var.ssl_enforcement_enabled
+//  ssl_enforcement           = var.ssl_enforcement_enabled
 //  ssl_minimal_tls_version_enforced  = var.ssl_minimal_tls_version_enforced
 }
