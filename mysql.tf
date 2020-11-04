330c381d6731c25b52d4d9ce9a826d9d4e0ec2c9
Charles Harvey <soops@ucla.edu>
2020-11-04 14:24:37 -0800

    testing omission of vars per tg validate take 5

--- mysql.tf
+++ mysql.tf
@@ -6,12 +6,12 @@ resource "azurerm_mysql_server" "mysql" {
   administrator_login_password = var.db_password
   sku_name = var.sku_name
   version = var.db_version
-  sku {
-    name     = "B_Gen5_2"
-    capacity = 2
-    tier     = "Basic"
-    family = "Gen5"
-  }
+//  sku {
+//    name     = "B_Gen5_2"
+//    capacity = 2
+//    tier     = "Basic"
+//    family = "Gen5"
+//  }
 
   storage_profile {
     storage_mb            = 5120
